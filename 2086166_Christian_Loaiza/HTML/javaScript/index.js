1ad3cc8f585f25a0effc6a17b23ff4ef1b2863e5
Christian Loaiza Garcia <chr.loaiza@gmail.com>
2021-09-07 20:22:04 -0500

    n

---
+++ 2086166_Christian_Loaiza/HTML/javaScript/index.js
@@ -0,0 +1,21 @@
+function namedFunction(event){
+  debugger
+  alert('Button Was clicked');
+}
+
+function blurInput(event){
+  debugger
+}
+/* const anonymousClick = document.getElementById('anonymous');
+anonymousClick.addEventListener(
+    'click',
+    function () {
+      alert ('Anonymous function');
+});
+
+const arrowClick = document.getElementById('arrow');
+arrowClick.addEventListener(
+    'click',
+    function () {
+      alert ('Arrow function');
+}); */
