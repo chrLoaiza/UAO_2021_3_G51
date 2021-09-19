11840789ec4eab835cc6e827e2a247c19ef15f24
jfchilito7 <jfchilito7@gmail.com>
2021-09-19 16:43:00 -0500

    ejercicio 2 js resuelto

---
+++ 2175175_Chilito_Juan/Ejericcio_2_JS_async/javascript/index.js
@@ -0,0 +1,37 @@
+/**
+ * This is the API URL to fetch data
+ * https://randomuser.me/api/?results=5
+ */
+
+document
+  .getElementById("apiButton")
+  .addEventListener("click", this.generateUsers.bind(this));
+
+  function generateUsers() {
+    const userSection = this.document.getElementById("users");
+    userSection.innerHTML = "";
+    this.fetchUserData().then((data) => {
+      const users = data.results;
+      for (let index = 0; index < users.length; index++) {
+        const element = users[index];
+        const userElement = `
+          <div class="user-container">
+            <div class="user-header">
+              <img src="${element.picture.large}" alt="medium_profile">
+            </div>
+            <div class="user-body">
+              <h3>${element.name.title}. ${element.name.first} ${element.name.last}</h3>
+              <p>${element.email}</p>
+              <p>${element.phone}</p>
+              <p>${element.location.city}, ${element.location.state} (${element.location.country})</p>
+            </div>
+          </div>`;
+        userSection.innerHTML += userElement;
+      }
+    });
+  }
+
+  async function fetchUserData() {
+    const response = await fetch("https://randomuser.me/api/?results=5");
+    return response.json();
+  }
