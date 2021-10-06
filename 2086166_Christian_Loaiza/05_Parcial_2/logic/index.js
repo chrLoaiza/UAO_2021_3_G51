c8c573b233a7eca03fb0d295b1da8b1d5196b554
Christian Loaiza Garcia <chr.loaiza@gmail.com>
2021-10-05 22:22:12 -0500

    Correcciones de estilo

--- 2086166_Christian_Loaiza/05_Parcial_2/logic/index.js
+++ 2086166_Christian_Loaiza/05_Parcial_2/logic/index.js
@@ -1,7 +1,6 @@
 const request_btn = document.getElementById("request-btn");
 request_btn.addEventListener("click", async () => {
   const cardList = document.getElementById("card-list");
-  const response = await fetch(
-    //realizar petición web
-  );
+  const response = await fetch();
+  //realizar petición web
 });
