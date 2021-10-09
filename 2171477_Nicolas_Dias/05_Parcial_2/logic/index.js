238601f4e05e022ba35bd067259cdb7a318bed00
nicolas diaz <nicolas_ale.diaz@uao.edu.co>
2021-10-08 22:19:16 -0500

    parcial 2

--- 2171477_Nicolas_Dias/05_Parcial_2/logic/index.js
+++ 2171477_Nicolas_Dias/05_Parcial_2/logic/index.js
@@ -1,6 +1,33 @@
-const request_btn = document.getElementById("request-btn");
-request_btn.addEventListener("click", async () => {
-  const cardList = document.getElementById("card-list");
-  const response = await fetch();
-  //realizar petición web
+document
+  .getElementById("request-btn") // llamando el boton 
+  .addEventListener("click", this.generatedogs.bind(this));
+  
+  function generatedogs(){
+    const dogsSection = this.document.getElementById("card-list"); // obtener el elemento users
+   
+    this.fetchDogsData().then((data) =>{ //llamado a la funcion fetch data, then es la promesa, se obtiene la data traida de la API
+      const dogs =data.message;    
+  
+   // con el for recorremos el arreglo y se crean las cartas      
+   for(let index = 0; index < dogs.length; index++){ // se captura toda la informacion de la API nombrem email, celular, locacion 
+    const element = dogs[index];                    // y se asigna de acuerdo al html 
+    const dogsElement = `
+    
+    <div class="dogs-container">
+      <div class="dogs-header">
+      <img src="${element}" id="foton">
+      <h3> Dogsito Random </h3>
+      </div>
+      <div class="dogs-body>
+      
+      </div>
+    </div>`;
+      dogsSection.innerHTML += dogsElement; // agregamos todo el html concatenando += 
+  }
 });
+}
+
+async function fetchDogsData(){ // funcion asincronica del fetch
+  const response = await fetch("https://dog.ceo/api/breeds/image/random/10"); // captura de la API
+  return response.json(); // retorna la promesa que seria el Json
+}
