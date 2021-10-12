c5abd16b086d2f38416653399b02e1477bbe5b94
LuisCollazosUAO <48070025+LuisCollazosUAO@users.noreply.github.com>
2021-10-11 22:13:52 -0500

    2171185_Collazos_Luis Parcial_2 (#63)
    * Update todo file
    
    * Update todo file
    
    * ADDING Parcial_1 directory
    
    * style modified
    
    * Parcial 2171185_Collazos_Luis
    
    * Update directories
    
    * Uploading Taller JS_1
    
    * Uploading Taller JS_2
    
    * Uploading Parcial_2
    
    Co-authored-by: Christian Loaiza Garcia <chr.loaiza@gmail.com>

---
+++ 2171185_Collazos_Luis/Ejericcio_2_JS_async/javascript/index.js
@@ -0,0 +1,40 @@
+/**
+ * This is the API URL to fetch data
+ * https://randomuser.me/api/?results=5
+ */
+
+document
+  .getElementById("apiButton")
+  .addEventListener("click", this.generateUsers.bind(this));
+
+function generateUsers() {
+  const users = this.requestAPIUser();
+  users.then((data) => {
+    const usersSection = document.getElementById("users");
+    const users = data.results;
+    for (let i=0; i < users.length; i++){
+      const userData = users[i];
+      const userElement = `
+      <div class="user-container">
+        <div class="user-header">
+          <img src="${userData.picture.large}" alt="medium_profile">
+        </div>
+        <div class="user-body">
+          <h3>${userData.name.title}. ${userData.name.first} ${userData.name.last}</h3>
+          <p>${userData.email}</p>
+          <p>${userData.phone}</p>
+          <p>${userData.location.city}, ${userData.location.state} ${userData.location.country}</p>
+        </div>
+      </div>
+      `;
+      usersSection.innerHTML += userElement;
+    }
+  })
+  
+  
+}
+
+async function requestAPIUser() {
+  const request = await fetch("https://randomuser.me/api/?results=5");
+  return request.json();
+}
