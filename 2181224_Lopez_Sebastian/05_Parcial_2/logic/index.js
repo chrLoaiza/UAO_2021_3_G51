32f1c0460b084e392ac6fdd1fa05d076b62087af
Sebastian Lopez Astudillo <71906722+Minotriz02@users.noreply.github.com>
2021-10-11 22:16:38 -0500

    2181224_Lopez_Sebastian Primer y segundo taller de JS y segundo parcial (#69)
    * Update todo file
    
    * Parcial_2181224_Lopez_Sebastian
    
    * First workshop JS
    
    * Workshop 2
    
    * Second test
    
    Co-authored-by: Christian Loaiza Garcia <chr.loaiza@gmail.com>
    Co-authored-by: Minotriz02 <sebastian.lopez_Ast@uao.edu.com>

---
+++ 2181224_Lopez_Sebastian/05_Parcial_2/logic/index.js
@@ -0,0 +1,32 @@
+const request_btn = document.getElementById("request-btn");
+request_btn.addEventListener("click", this.generateDogs.bind(this));
+
+function generateDogs(){
+  const dogs = this.requestAPIDogs();
+  dogs.then((data)=>{
+    const cardList = document.getElementById("card-list");
+    const dogs = data.message;
+    for (let i = 0; i < dogs.length; i++) {
+      const dogData = dogs[i];
+      
+      const dogEle = `
+        <div class="card">
+          <img src="${dogData}" class="card_image" alt="" />
+          <div>
+            <div class="card__header">
+              <img class="card__thumb" src="${dogData}" alt="" />
+              <div>
+                <h3 class="card__title">Randon Dog ${i+1}</h3>
+              </div>
+            </div>
+          </div>
+        </div>`;
+      cardList.innerHTML += dogEle
+    }
+  })
+}
+
+async function requestAPIDogs(){
+  const response = await fetch('https://dog.ceo/api/breeds/image/random/10');
+  return response.json();
+}
