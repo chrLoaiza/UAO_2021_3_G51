9a9f08b529b37809ca2949544f5ce463ba48bf74
FernandoPareja <69766482+FernandoPareja@users.noreply.github.com>
2021-10-11 22:14:24 -0500

    2185833_Pareja_Luis Taller JS (#64)
    * Update todo file
    
    * Parcial1 Finished
    
    * Delete
    
    * Parcial 2185833_Pareja_Luis
    
    * 'JSWorkshop_218583'
    
    * JSWorkshop2_2185833
    
    * JSWorkshop2_2185833
    
    * Parcial2_2185833_Pareja_Luis
    
    Co-authored-by: Christian Loaiza Garcia <chr.loaiza@gmail.com>

---
+++ 2185833_Pareja_Luis/Ejericcio_2_JS_async/javascript/index.js
@@ -0,0 +1,40 @@
+/**
+ * This is the API URL to fetch data
+ * https://randomuser.me/api/?results=5
+ */
+
+document
+  .getElementById("apiButton")
+  .addEventListener("click", this.generateUsers.bind(this));
+
+  function generateUsers(){
+    const userSection = this.document.getElementById("users");
+    userSection.innerHTML = "";
+
+    this.fetchUserData().then((data) => {
+      const users = data.results;
+      for (let index = 0; index < users.length; index++){
+        const userData = users[index];
+        const userElement = `
+        <div class="user-container">
+
+          <div class="user-body">
+            <h3>${userData.name.title}. ${userData.name.first} ${userData.name.last}</h3>
+            <p>${userData.email}</p>
+            <p>${userData.phone}</p>
+            <p>${userData.location.city}, ${userData.location.state}, ${userData.location.country}</p>
+          </div>
+        </div>`;
+        userSection.innerHTML += userElement;
+
+      }
+
+    });
+  }
+
+ 
+  async function fetchUserData(){
+    const response = await fetch("https://randomuser.me/api/?results=5");
+    return response.json();
+
+  }
