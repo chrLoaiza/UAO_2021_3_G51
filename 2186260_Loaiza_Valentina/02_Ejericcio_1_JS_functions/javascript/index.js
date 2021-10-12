21eaac7a974566fbb30915906ba915f9e0bcf3b0
valenlzmj <64485118+valenlzmj@users.noreply.github.com>
2021-10-11 22:05:04 -0500

    2186260 Loaiza Valentina Parcial (#74)
    * Update todo file
    
    * copia carpeta
    
    * Parcial 2186260_Loaiza_Valentina
    
    * 2186260_Loaiza_Valentina_esteSi
    
    * Update directories
    
    * taller2
    
    * Parcial 2
    
    * Taller1
    
    Co-authored-by: Christian Loaiza Garcia <chr.loaiza@gmail.com>

---
+++ 2186260_Loaiza_Valentina/02_Ejericcio_1_JS_functions/javascript/index.js
@@ -0,0 +1,34 @@
+// const functionBtn = document.getElementById("namedFunction");
+
+// functionBtn.addEventListener("click", this.buttonClicked);
+
+function dobleClick(event) {
+  // alert('I was double clicked, yaaay!!!!!!!!!');
+}
+
+function buttonClicked(event) {
+  // alert("Normal Function");
+}
+
+function formSubmited(event) {
+  let element = document.getElementById('form').children
+  const loopSize = element.length;
+  let mailData = {}
+  for (let index = 0; index < loopSize; index++) {
+    const element = document.getElementById('form').children[index];
+    const inputEle = element.children[1];
+    mailData[inputEle.name] = inputEle.value;
+  }
+  alert("holi");
+}
+
+function focusE(event){
+  element = event.srcElement;
+  element.classList.toggle("focus", true);
+}
+
+function blurE(event){
+  element = event.srcElement;
+  element.classList.toggle("focus", false);
+}
+
