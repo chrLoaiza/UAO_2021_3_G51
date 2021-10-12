8a9a151b8502ec59fb53bf694e661700fc8b7817
Brian León Hoyos <69739801+Brianleonhoyos@users.noreply.github.com>
2021-10-11 22:14:45 -0500

    2180428_Brian_Leon_Taller2 (#65)
    * Update todo file
    
    * Solucion Parcial 1
    
    Envio solucion sobre el punto practico
    
    * Create index.html
    
    * Taller 2
    
    * Ejercicio 3
    
    Desarrollo de la tarea 3, hacer uso de una api y utilizamos los datos
    
    * Parcial 2
    
    * Parcial 2 Finalizado
    
    Entrega final parcial 2
    
    * Taller 2
    
    Corrección taller 2
    
    * Parcial final
    
    Modificacion css
    
    Co-authored-by: Christian Loaiza Garcia <chr.loaiza@gmail.com>

---
+++ 2180428_Brian_Leon/Ejercicios/Ejercicio3/javascript/index.js
@@ -0,0 +1,35 @@
+document
+  .getElementById("apiButton")
+  .addEventListener("click", this.generateUsers.bind(this));
+
+function generateUsers() {
+  const usersSections = this.document.getElementById("users");
+  usersSections.innerHTML = "";
+  this.fetchUsersData().then((data) => {
+    /* debugger */
+    const users = data.results;
+    for (let index = 0; index < users.length; index++) {
+      const element = users[index];
+      const userElement =
+        `
+        <div id="contenedor" class="user-container">
+        <div class="user-header">
+        <img src="${element.picture.large}" width="100px" class="img-fluid rounded-circle">
+        </div>
+        <div class="user-body">
+        <p><b>Nombre: </b>${element.name.title} ${element.name.first} ${element.name.last}</p>
+        <p><b>Correo Electronico: </b> ${element.email}</p>
+        <p><b>Telefono: </b>${element.phone}</p>
+        <p><b>Direccion: </b>${element.location.city} ${element.location.state}</p>
+        </div>
+        </div>
+        `
+      usersSections.innerHTML += userElement;
+    }
+  });
+}
+/* alert('Error'); */
+async function fetchUsersData() {
+  const response = await fetch("https://randomuser.me/api/?results=12");
+  return response.json();
+}
